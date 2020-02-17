facb374db351c0b4e8cb56e8a3d0a627e2d01c42
Kevin Pliester <me@pixelbart.de>
2020-02-17 17:02:40 +0100

    4.2.7

--- helpful.php
+++ helpful.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Helpful
  * Description: Add a fancy feedback form under your posts or post-types and ask your visitors a question. Give them the abbility to vote with yes or no.
- * Version: 4.2.6
+ * Version: 4.2.7
  * Author: Pixelbart
  * Author URI: https://pixelbart.de
  * Text Domain: helpful
