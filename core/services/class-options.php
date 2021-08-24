7bb81db5d53d8ef8017f488399741d69a68a28a8
Kevin Pliester <5339107+pixelbart@users.noreply.github.com>
2021-08-24 21:20:11 +0200

    4.4.47
    Classes for options and also to create XML files for WPML but also for export.

---
+++ core/services/class-options.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @package Helpful\Core\Services
+ * @author  Pixelbart <me@pixelbart.de>
+ * @version 4.4.47
+ */
+namespace Helpful\Core\Services;
+
+use Helpful\Core\Helper;
+use Helpful\Core\Helpers as Helpers;
+
+/* Prevent direct access */
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Options
+{
+    /**
+     * @var array
+     */
+    private $options;
+
+    /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $options = maybe_unserialize(get_option('helpful_options'));
+        $this->options = (!is_array($options)) ? [] : $options;
+    }
+
+    /**
+     * @return void
+     */
+    public function renew_options()
+    {
+        update_option('helpful_options', maybe_serialize($this->options));
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     */
+    public function update_option($name, $value)
+    {
+        if (!is_string($name)) {
+            return;
+        }
+
+        $this->options[$name] = $value;
+        $this->renew_options();
+    }
+
+    /**
+     * @param string $name
+     * @return void
+     */
+    public function delete_option($name)
+    {
+        if (!is_string($name)) {
+            return;
+        }
+
+        if (isset($this->options[$name])) {
+            unset($this->options[$name]);
+        }
+
+        $this->renew_options();
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $default
+     * @return mixed
+     */
+    public function get_option($name, $default = false) {
+        if (!is_string($name)) {
+            return $default;
+        }
+
+        if (isset($this->options[$name])) {
+            return $this->options[$name];
+        }
+
+        return $default;
+    }
+
+    /**
+     * @return array
+     */
+    public function get_options()
+    {
+        return $this->options;
+    }
+}
---
+++ core/services/class-xml.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package Helpful\Core\Services
+ * @author  Pixelbart <me@pixelbart.de>
+ * @version 4.4.47
+ */
+namespace Helpful\Core\Services;
+
+use Helpful\Core\Helper;
+use Helpful\Core\Helpers as Helpers;
+
+/* Prevent direct access */
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class XML
+{
+    /**
+     * @var string
+     */
+    private $filename;
+
+    /**
+     * @var array
+     */
+    private $lists;
+
+    /**
+     * @param string $filename
+     * @return void
+     */
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+        $this->lists = [];
+    }
+
+    /**
+     * @param string $listname
+     * @return void
+     */
+    public function add_list($listname)
+    {
+        $this->lists[$listname] = [];
+    }
+
+    /**
+     * @param string $listname
+     * @param string $item
+     * @return void
+     */
+    public function add_list_item($listname, $item)
+    {
+        $this->lists[$listname][] = $item;
+    }
+
+    /**
+     * @return string
+     */
+    public function get_file()
+    {
+        return HELPFUL_PATH . $this->filename . '.xml';
+    }
+
+    /**
+     * @return void
+     */
+    public function save()
+    {
+        $file = fopen($this->get_file(), "w");
+        fwrite($file, sprintf('<%s>', $this->filename) . "\n");
+
+        if (!empty($this->lists)) {
+            foreach ($lists as $name => $items) {
+                if (empty($items)) {
+                    continue;
+                }
+
+                fwrite($file, sprintf('<%s>', $name) . "\n");                
+                foreach ($items as $item) :
+                    fwrite($file, $item . "\n");                    
+                endforeach;
+                fwrite($file, sprintf('</%s>', $name) . "\n");
+            }
+        }
+
+        fwrite($file, sprintf('</%s>', $this->filename));
+        fclose($file);
+    }
+}
